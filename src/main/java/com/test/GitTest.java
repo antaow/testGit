2342340bf268f8c33353b0cdcb2d2019abbc8f31
atw <antaow@outlook.com>
2026-01-29 23:54:08 +0800

    hot-fix commit1

--- src/main/java/com/test/GitTest.java
+++ src/main/java/com/test/GitTest.java
@@ -11,5 +11,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hot-fix git");
+        System.out.println("master git");
+        System.out.println("hot-fix git2");
     }
 }
