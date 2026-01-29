b0ad38843e6d6d120fae17c033de16b754f6dac4
atw <antaow@outlook.com>
2026-01-29 23:22:28 +0800

    first commit

---
+++ src/main/java/com/test/GitTest.java
@@ -0,0 +1,14 @@
+package com.test;
+
+/**
+ * @time: 2026/1/29 23:05
+ * @author: antaowang
+ * @file: GitTest.java
+ */
+
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
